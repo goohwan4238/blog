552c3d1353965fc28b8a22c02b543cc5a1f7f830
Chung goohwan <41721728+goohwan4238@users.noreply.github.com>
2023-07-11 09:10:02 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -10,12 +10,7 @@ const CONFIG = {
     github: "",
     instagram: "",
   },
-  projects: [
-    {
-      name: `vms-solution log`,
-      href: "https://github.com/goohwan4238/blog",
-    },
-  ],
+
   // blog setting (required)
   blog: {
     title: "VMS SOLUTIONS Blog",
